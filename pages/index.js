5e1be5e776b3e93d1db4f119fdc8787a7197cc05
MasterAddy <adesileisaiah70@gmail.com>
2021-11-06 20:19:35 -0700

    Designed home and reg page

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,94 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Captcha from "demos-react-captcha";
+import Router from "next/router";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div
+      style={{ background: "url('/bg.jpg')", backgroundSize: "cover" }}
+      className="flex flex-col items-center justify-center min-h-screen py-2 bg-green-600"
+    >
       <Head>
-        <title>Create Next App</title>
+        <title>PDP Membership Registration</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
+      <main className="container grid items-center justify-center flex-1 w-full grid-cols-1 px-3 md;px-10 lg:px-20 mx-auto text-center md:grid-cols-2">
+        <div className="flex flex-col justify-center mt-5 md:hidden">
+          <img src="/log.png" className="w-24 h-24 mx-auto" alt="" />
+          <div className="uppercase">
+            <h1 className="text-xl font-bold text-green-50">Peoples Democratic party</h1>
+            <p className="text-sm font-bold text-green-50">membership management system</p>
+            <p className="mt-6 text-2xl font-bold leading-none text-green-50">
+              join the largest <br /> political <span className="text-red-600">party</span> <br />{" "}
+              in nigeria
             </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <h1 className="hidden text-3xl font-bold uppercase text-green-50 md:block">
+            Registration
+          </h1>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              Router.push("/registration");
+            }}
+            className="max-w-md p-5 py-10 mt-5 rounded-lg bg-green-50"
           >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
+            <h2 className="mb-6 text-xl font-bold text-green-800 uppercase md:hidden">Sign Up</h2>
+            <input
+              type="text"
+              className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+              placeholder="Surname"
+            />
+            <input
+              type="text"
+              className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+              placeholder="Firstname"
+            />
+            <input
+              type="text"
+              className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+              placeholder="Othername"
+            />
+            <div className="flex justify-center mb-4">
+              <Captcha
+                onChange={(status) => console.log(status)}
+                onRefresh={(status) => console.log(status)}
+                placeholder="Insert Captcha"
+                length={6}
+              />
+            </div>
+            <button className="px-2 py-1 text-green-700 uppercase border-2 border-green-700 rounded-md hover:bg-green-500 hover:text-green-100">
+              Proceed
+            </button>
+          </form>
+        </div>
+        <div className="flex-col justify-center hidden md:flex">
+          <div className="flex flex-col uppercase">
+            <p className="justify-around mt-6 mb-24 text-4xl font-bold leading-none text-green-50">
+              join the largest <br /> political <span className="text-red-600">party</span> <br />{" "}
+              in nigeria
             </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <div className="flex flex-wrap justify-center mt-24">
+              <img src="/logo.png" className="" alt="" />
+              <div className="flex flex-col justify-start pl-2 text-4xl font-bold text-green-600">
+                <p className="md:text-left">Peoples</p>
+                <p className="md:text-left">Democratic</p>
+                <p className="text-red-600 md:text-left">party</p>
+              </div>
+            </div>
+          </div>
         </div>
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <div className="max-w-lg px-2 mt-5 text-xs text-center text-green-800 md:text-white">
+        <p className="font-semibold uppercase">Please Note:</p>
+        <p className="">
+          Please ensure the names entered above are spelt correctly because they cannot be changed
+          once you proceed beyond this point. Click on "Proceed" button below to proceed.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
---
+++ pages/registration.js
@@ -0,0 +1,231 @@
+import Head from "next/head";
+// import Captcha from "demos-react-captcha";
+
+export default function Registration() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-green-600">
+      <Head>
+        <title>PDP Membership Registration</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="container flex flex-col items-center justify-center w-full px-3 md;px-10 lg:px-20 mx-auto text-center md:grid-cols-2">
+        <div className="flex flex-col justify-center mt-5">
+          <div className="uppercase">
+            <div className="flex flex-wrap items-center justify-center">
+              <img src="/log.png" className="w-24 h-24" alt="" />
+              <div>
+                <h1 className="text-xl font-bold text-green-50">Peoples Democratic party</h1>
+                <p className="text-sm font-bold text-green-50">membership management system</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          {/* <h1 className="hidden text-3xl font-bold uppercase text-green-50 md:block">
+            Registration
+          </h1> */}
+
+          <form action="" className="max-w-lg p-5 py-10 mt-5 rounded-lg bg-green-50">
+            <h2 className="mb-6 text-xl font-bold text-green-800 uppercase md:hidden">
+              Person information
+            </h2>
+            <div className="grid grid-cols-2 gap-2 text-xs">
+              <div className="text-left">
+                <label htmlFor="" className="w-full">
+                  Title <span className="font-bold text-green-600">*</span>
+                  <select
+                    name=""
+                    className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                    id=""
+                  >
+                    <option value="">Please Select</option>
+                    <option value="mr">Mr</option>
+                    <option value="mr">Mrs</option>
+                    <option value="mr">Miss</option>
+                  </select>
+                </label>
+                <label htmlFor="">
+                  Name <span className="font-bold text-green-600">*</span>
+                  <input
+                    type="text"
+                    className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                    placeholder="Firstname"
+                  />
+                </label>
+                <label htmlFor="">
+                  Surname <span className="font-bold text-green-600">*</span>
+                  <input
+                    type="text"
+                    className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                    placeholder="Surname"
+                  />
+                </label>
+                <label htmlFor="">
+                  Othernames <span className="font-bold text-green-600">*</span>
+                  <input
+                    type="text"
+                    className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                    placeholder="Othername"
+                  />
+                </label>
+                <label htmlFor="" className="w-full">
+                  Gender <span className="font-bold text-green-600">*</span>
+                  <select
+                    name=""
+                    className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                    id=""
+                  >
+                    <option value="">Please Select</option>
+                    <option value="m">Male</option>
+                    <option value="f">Female</option>
+                  </select>
+                </label>
+                <label htmlFor="" className="w-full">
+                  Date Of Birth <span className="font-bold text-green-600">*</span>
+                  <input
+                    type="date"
+                    className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                    placeholder="Date of Birth"
+                  />
+                </label>
+                <label htmlFor="">
+                  Phone Number <span className="font-bold text-green-600">*</span>
+                  <input
+                    type="text"
+                    className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                    placeholder="09012345678"
+                  />
+                </label>
+                <label htmlFor="">
+                  Phone Network <span className="font-bold text-green-600">*</span>
+                  <select
+                    name=""
+                    className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                    id=""
+                  >
+                    <option value="">Please Select</option>
+                    <option value="m">MTN</option>
+                    <option value="a">Airtel</option>
+                    <option value="e">Etisalat (9ja)</option>
+                    <option value="g">GLO</option>
+                  </select>
+                </label>
+              </div>
+              <div className="text-left">
+                <img src="/profile.png" className="h-36 w-28" alt="" />
+                <input
+                  type="file"
+                  className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                  // placeholder="Upload"
+                />
+                <label htmlFor="">
+                  Year of Joining <span className="font-bold text-green-600">*</span>
+                  <select
+                    name=""
+                    className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                    id=""
+                  >
+                    <option value="">Please Select</option>
+                    <option value="10">2015</option>
+                    <option value="11">2016</option>
+                    <option value="12">2017</option>
+                    <option value="1">2018</option>
+                    <option value="2">2019</option>
+                    <option value="3">2020</option>
+                    <option value="4">2021</option>
+                  </select>
+                </label>
+                <label htmlFor="">
+                  Email <span className="font-bold text-green-600">*</span>
+                  <input
+                    type="email"
+                    className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                    placeholder="name@domain.com"
+                  />
+                </label>
+                <label htmlFor="">
+                  State <span className="font-bold text-green-600">*</span>
+                  <select
+                    name=""
+                    className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                    id=""
+                  >
+                    <option value="">Please Select</option>
+                    <option value="10">2015</option>
+                    <option value="11">2016</option>
+                    <option value="12">2017</option>
+                    <option value="1">2018</option>
+                    <option value="2">2019</option>
+                    <option value="3">2020</option>
+                    <option value="4">2021</option>
+                  </select>
+                </label>
+                <label htmlFor="">
+                  Local Government <span className="font-bold text-green-600">*</span>
+                  <select
+                    name=""
+                    className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                    id=""
+                  >
+                    <option value="">Please Select</option>
+                    <option value="10">2015</option>
+                    <option value="11">2016</option>
+                    <option value="12">2017</option>
+                    <option value="1">2018</option>
+                    <option value="2">2019</option>
+                    <option value="3">2020</option>
+                    <option value="4">2021</option>
+                  </select>
+                </label>
+                <label htmlFor="">
+                  Ward <span className="font-bold text-green-600">*</span>
+                  <select
+                    name=""
+                    className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                    id=""
+                  >
+                    <option value="">Please Select</option>
+                    <option value="10">2015</option>
+                    <option value="11">2016</option>
+                    <option value="12">2017</option>
+                    <option value="1">2018</option>
+                    <option value="2">2019</option>
+                    <option value="3">2020</option>
+                    <option value="4">2021</option>
+                  </select>
+                </label>
+              </div>
+            </div>
+            <button className="px-2 py-1 text-green-700 uppercase border-2 border-green-700 rounded-md hover:bg-green-500 hover:text-green-100">
+              Proceed
+            </button>
+          </form>
+        </div>
+        <div className="flex-col justify-center hidden">
+          <div className="flex flex-col uppercase">
+            <p className="justify-around mt-6 mb-24 text-4xl font-bold leading-none text-green-50">
+              join the largest <br /> political <span className="text-red-600">party</span> <br />{" "}
+              in nigeria
+            </p>
+            <div className="flex flex-wrap justify-center mt-24">
+              <img src="/vercel.svg" className="" alt="" />
+              <div className="flex flex-col justify-start pl-2 text-4xl font-bold text-green-50">
+                <p className="md:text-left">Peoples</p>
+                <p className="md:text-left">Democratic</p>
+                <p className="text-red-600 md:text-left">party</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <div className="max-w-lg px-2 mt-5 text-xs text-center text-white">
+        <p className="font-semibold uppercase">Please Note:</p>
+        <p className="">
+          Please ensure the names entered above are spelt correctly because they cannot be changed
+          once you proceed beyond this point. Click on "Proceed" button below to proceed.
+        </p>
+      </div>
+    </div>
+  );
+}
