8eb7ddeb77bc01012027089103c3e7bbbf727244
MasterAddy <adesileisaiah70@gmail.com>
2021-11-24 05:10:50 -0800

    Update Dashboard

--- components/AgentDashboard.js
+++ components/AgentDashboard.js
@@ -3,18 +3,16 @@ import React from "react";
 const AgentDashboard = () => {
   return (
     <div className="flex">
-      <div className="relative min-h-screen w-36">
+      <div className="relative hidden min-h-screen md:block w-36">
         <img src="/log.png" alt="" className="w-3/5 mx-auto my-6" />
         <img src="/profile.png" alt="" className="w-4/6 mx-auto" />
-        <h2 className="mx-auto font-bold text-center text-green-700 uppercase">
-          Ahmad Bello
-        </h2>
+        <h2 className="mx-auto font-bold text-center text-green-700 uppercase">Ahmad Bello</h2>
         <h2 className="p-2 mx-auto my-5 font-bold leading-4 text-green-700 uppercase">
           overall reg: <br />
           234
         </h2>
         <div className="flex flex-col my-6">
-          <img src="/settings.png" alt="" className="w-3/5 mx-auto" />
+          <img src="/settings.png" alt="" className="w-1/5 mx-auto" />
           <p className="font-bold text-center text-green-700">Settings</p>
         </div>
         <div className="absolute flex items-center justify-center bottom-4 left-5">
@@ -22,17 +20,35 @@ const AgentDashboard = () => {
           <p className="text-sm font-medium text-center">Log Out</p>
         </div>
       </div>
-      <div className="w-full min-h-screen bg-green-300">
-        <div className="text-center uppercase">
+
+      <div className="w-full min-h-screen bg-green-100">
+        <div className="mt-5 text-center uppercase">
           <h1 className="text-2xl font-bold text-green-800">
             Peoples Democratic <span className="text-red-700"> party</span>
           </h1>
-          <p className="font-semibold text-green-600">
-            Membership management system dashboard
-          </p>
+          <p className="font-semibold text-green-600">Membership management system dashboard</p>
         </div>
-        <div className="flex">
-          <div className="flex flex-col w-1/4 p-2 m-2 text-green-800">
+        <div className="block p-5 my-8 rounded bg-green-50 md:hidden">
+          <div className="flex items-center justify-center mx-auto">
+            <img src="/profile.png" alt="" className="w-12 h-12 rounded-full" />
+            <h2 className="font-bold text-center text-green-700 uppercase">Ahmad Bello</h2>
+          </div>
+          <h2 className="p-2 mx-auto my-2 font-bold leading-4 text-center text-green-700 uppercase">
+            overall reg: 234
+          </h2>
+          <div className="flex items-center justify-around">
+            <div className="flex items-center justify-center">
+              <img src="/logout.png" alt="" className="w-8 mr-2" />
+              <p className="text-sm font-medium text-center">Log Out</p>
+            </div>
+            <div className="flex items-center justify-center">
+              <img src="/settings.png" alt="" className="w-8 mr-2" />
+              <p className="font-bold text-center text-green-700">Settings</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row">
+          <div className="flex flex-col p-2 m-2 text-sm text-green-800 md:w-1/4 lg:text-base">
             <p className="text-5xl font-bold text-green-800">On Going</p>
             <p className="my-4 text-green-800">Ongoing Registration</p>
             <div className="flex items-center justify-center p-2">
@@ -46,20 +62,18 @@ const AgentDashboard = () => {
                 <p className="px-2 mx-auto">Pending Enrollment</p>
                 <p className="p-2 border-l border-green-600">123</p>
               </div>
-              <a className="absolute right-4 bottom-4">
-                <img src="/arrow.png" alt="" className="w-8 h-8" />
+              <a className="absolute right-2 bottom-2">
+                <img src="/arrow.png" alt="" className="w-4 h-4" />
               </a>
             </div>
           </div>
-          <div className="">
-            <form
-              action=""
-              className="max-w-lg p-5 py-10 mt-5 rounded-lg bg-green-50"
-            >
+
+          <div className="p-5 md:w-3/4">
+            <form action="" className="p-5 py-10 mt-5 rounded-lg bg-green-50">
               <h2 className="mb-6 text-xl font-bold text-green-800 uppercase md:hidden">
                 Person information
               </h2>
-              <div className="grid grid-cols-3 gap-2 text-xs">
+              <div className="grid grid-cols-1 gap-2 text-xs md:grid-cols-3">
                 <div className="text-left">
                   <label htmlFor="" className="w-full">
                     Title <span className="font-bold text-green-600">*</span>
@@ -91,8 +105,7 @@ const AgentDashboard = () => {
                     />
                   </label>
                   <label htmlFor="">
-                    Othernames{" "}
-                    <span className="font-bold text-green-600">*</span>
+                    Othernames <span className="font-bold text-green-600">*</span>
                     <input
                       type="text"
                       className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
@@ -112,8 +125,7 @@ const AgentDashboard = () => {
                     </select>
                   </label>
                   <label htmlFor="" className="w-full">
-                    Date Of Birth{" "}
-                    <span className="font-bold text-green-600">*</span>
+                    Date Of Birth <span className="font-bold text-green-600">*</span>
                     <input
                       type="date"
                       className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
@@ -121,8 +133,7 @@ const AgentDashboard = () => {
                     />
                   </label>
                   <label htmlFor="">
-                    Phone Number{" "}
-                    <span className="font-bold text-green-600">*</span>
+                    Phone Number <span className="font-bold text-green-600">*</span>
                     <input
                       type="text"
                       className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
@@ -130,8 +141,7 @@ const AgentDashboard = () => {
                     />
                   </label>
                   <label htmlFor="">
-                    Phone Network{" "}
-                    <span className="font-bold text-green-600">*</span>
+                    Phone Network <span className="font-bold text-green-600">*</span>
                     <select
                       name=""
                       className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
@@ -153,8 +163,7 @@ const AgentDashboard = () => {
                     // placeholder="Upload"
                   />
                   <label htmlFor="">
-                    Year of Joining{" "}
-                    <span className="font-bold text-green-600">*</span>
+                    Year of Joining <span className="font-bold text-green-600">*</span>
                     <select
                       name=""
                       className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
@@ -196,8 +205,7 @@ const AgentDashboard = () => {
                     </select>
                   </label>
                   <label htmlFor="">
-                    Local Government{" "}
-                    <span className="font-bold text-green-600">*</span>
+                    Local Government <span className="font-bold text-green-600">*</span>
                     <select
                       name=""
                       className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
@@ -262,8 +270,7 @@ const AgentDashboard = () => {
                     />
                   </label>
                   <label htmlFor="">
-                    Othernames{" "}
-                    <span className="font-bold text-green-600">*</span>
+                    Othernames <span className="font-bold text-green-600">*</span>
                     <input
                       type="text"
                       className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
@@ -283,8 +290,7 @@ const AgentDashboard = () => {
                     </select>
                   </label>
                   <label htmlFor="" className="w-full">
-                    Date Of Birth{" "}
-                    <span className="font-bold text-green-600">*</span>
+                    Date Of Birth <span className="font-bold text-green-600">*</span>
                     <input
                       type="date"
                       className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
@@ -292,8 +298,7 @@ const AgentDashboard = () => {
                     />
                   </label>
                   <label htmlFor="">
-                    Phone Number{" "}
-                    <span className="font-bold text-green-600">*</span>
+                    Phone Number <span className="font-bold text-green-600">*</span>
                     <input
                       type="text"
                       className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
@@ -301,8 +306,7 @@ const AgentDashboard = () => {
                     />
                   </label>
                   <label htmlFor="">
-                    Phone Network{" "}
-                    <span className="font-bold text-green-600">*</span>
+                    Phone Network <span className="font-bold text-green-600">*</span>
                     <select
                       name=""
                       className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
--- components/CallCenDashboard.js
+++ components/CallCenDashboard.js
@@ -1,182 +1,138 @@
 import React from "react";
 
-const CallCenDashboard = () => {
+const AgentDashboard = () => {
   return (
-    <div className="bg-gray-50 flex flex-col min-h-screen py-2">
-      <h2 className="text-green-700 text-2xl font-bold uppercase text-center pl-32">
-        People democratic <span className="text-red-700">party</span>{" "}
-      </h2>
-      <div className="flex mx-4">
-        <div className="relative rounded-l-3xl bg-white min-h-screen w-36">
-          <img src="/log.png" alt="" className="w-2/5 mx-auto my-6" />
-          <img src="/pic.jpg" alt="" className="w-3/6 mx-auto" />
-          <h2 className="mx-auto font-bold text-xs text-center text-green-700 uppercase">
-            Ahmad Bello
-          </h2>
-          <h2 className="p-2 mx-auto my-5 mt-20 font-bold text-sm leading-4 text-green-700 uppercase">
-            overall reg: <br />
-            234
-          </h2>
-          <div className="flex flex-col my-auto">
-            <img src="/settings.png" alt="" className="w-1/5 mx-auto" />
-            <p className="font-bold text-center text-sm text-green-700">
-              Settings
-            </p>
+    <div className="flex">
+      <div className="relative hidden min-h-screen md:block w-36">
+        <img src="/log.png" alt="" className="w-3/5 mx-auto my-6" />
+        <img src="/profile.png" alt="" className="w-4/6 mx-auto rounded-lg shadow-lg" />
+        <h2 className="mx-auto font-bold text-center text-green-700 uppercase">Ahmad Bello</h2>
+        <h2 className="p-2 mx-auto my-5 font-bold leading-4 text-green-700 uppercase">
+          overall reg: <br />
+          234
+        </h2>
+        <div className="flex flex-col my-6">
+          <img src="/settings.png" alt="" className="w-1/5 mx-auto" />
+          <p className="font-bold text-center text-green-700">Settings</p>
+        </div>
+        <div className="absolute flex items-center justify-center bottom-4 left-5">
+          <img src="/logout.png" alt="" className="w-8 pr-2" />
+          <p className="text-sm font-medium text-center">Log Out</p>
+        </div>
+      </div>
+
+      <div className="w-full min-h-screen bg-green-100">
+        <div className="mt-5 text-center uppercase">
+          <h1 className="text-2xl font-bold text-green-800">
+            Peoples Democratic <span className="text-red-700"> party</span>
+          </h1>
+          <p className="font-semibold text-green-600">Membership management system dashboard</p>
+        </div>
+        <div className="block p-5 my-8 rounded bg-green-50 md:hidden">
+          <div className="flex items-center justify-center mx-auto">
+            <img src="/profile.png" alt="" className="w-12 h-12 rounded-full" />
+            <h2 className="font-bold text-center text-green-700 uppercase">Ahmad Bello</h2>
           </div>
-          <div className="absolute flex items-center justify-center bottom-4 left-5">
-            <img src="/logout.png" alt="" className="w-1/5" />
-            <p className="text-sm font-medium text-center">Log Out</p>
+          <h2 className="p-2 mx-auto my-2 font-bold leading-4 text-center text-green-700 uppercase">
+            overall reg: 234
+          </h2>
+          <div className="flex items-center justify-around">
+            <div className="flex items-center justify-center">
+              <img src="/logout.png" alt="" className="w-8 mr-2" />
+              <p className="text-sm font-medium text-center">Log Out</p>
+            </div>
+            <div className="flex items-center justify-center">
+              <img src="/settings.png" alt="" className="w-8 mr-2" />
+              <p className="font-bold text-center text-green-700">Settings</p>
+            </div>
           </div>
         </div>
-        <div className="w-full rounded-r-3xl min-h-screen bg-green-100">
-          <div className="uppercase">
-            <p className="font-semibold text-center text-green-600">
-              Membership management system dashboard
-            </p>
-          </div>
-          <div className="flex">
-            <div className="flex flex-col w-1/4 p-2 m-2 mx-10 text-green-800">
-              <p className="text-5xl font-bold text-green-800">
-                On <br /> Going
-              </p>
-              <p className="my-4 text-green-800">Ongoing Registration</p>
-              <div className="flex py-2">
-                <img src="/task.png" alt="" className="w-8 h-8" />
-                <p className="mx-6 text-sm italic">
-                  Completed
-                  <br /> Enrollment
-                </p>
-                <p className="p-2 border-l border-green-600">240</p>
+        <div className="flex flex-col md:flex-row">
+          <div className="flex flex-col p-2 m-2 text-sm text-green-800 md:w-1/4 lg:text-base">
+            <p className="text-5xl font-bold text-green-800">On Going</p>
+            <p className="my-4 text-green-800">Ongoing Registration</p>
+            <div className="flex items-center justify-center p-2">
+              <img src="/task.png" alt="" className="w-8 h-8" />
+              <p className="px-2 mx-auto">Completed Enrollment</p>
+              <p className="p-2 border-l border-green-600">123</p>
+            </div>
+            <div className="relative p-2 rounded-lg shadow-xl bg-green-50">
+              <div className="flex items-center justify-center">
+                <img src="/task1.png" alt="" className="w-8 h-8" />
+                <p className="px-2 mx-auto">Pending Enrollment</p>
+                <p className="p-2 border-l border-green-600">123</p>
               </div>
-              <div className="relative rounded-lg shadow-xl h-96 max-w-lg bg-white">
-                <div className="flex mx-auto p-2">
-                  <img src="/task1.png" alt="" className="w-8 h-8" />
-                  <p className="mx-6 text-sm italic">
-                    Pending
-                    <br /> Enrollment
-                  </p>
-                  <p className="p-2 border-l border-green-600">123</p>
+              <a className="absolute right-2 bottom-2">
+                <img src="/arrow.png" alt="" className="w-4 h-4" />
+              </a>
+              <div className="flex flex-col items-center justify-center font-bold">
+                <div className="h-64 px-2 overflow-y-scroll">
+                  {"123456789".split("").map((value) => (
+                    <div className="flex py-2 border-b broder-green-600">
+                      <span className="pr-1"> {value}</span>
+                      <span className="pl-1 border-l border-green-600"> Hammed Bellow</span>
+                    </div>
+                  ))}
                 </div>
-                <div className="flex flex-col font-bold items-center justify-center">
-                  <div className="border-l border-green-600 m-2 px-2">
-                    <div className="border-b broder-green-600">
-                      Hammed Bellow
-                    </div>
-                    <div className="border-b broder-green-600">
-                      Hammed Bellow
-                    </div>
-                    <div className="border-b broder-green-600">
-                      Hammed Bellow
-                    </div>
-                    <div className="border-b broder-green-600">
-                      Hammed Bellow
-                    </div>
-                    <div className="border-b broder-green-600">
-                      Hammed Bellow
-                    </div>
-                    <div className="border-b broder-green-600">
-                      Hammed Bellow
-                    </div>
-                    <div className="border-b broder-green-600">
-                      Hammed Bellow
-                    </div>
-                    <div className="border-b broder-green-600">
-                      Hammed Bellow
-                    </div>
-                    <div className="border-b broder-green-600">
-                      Hammed Bellow
-                    </div>
-                    <div className="border-b broder-green-600">
-                      Hammed Bellow
-                    </div>
-                    <div className="border-b broder-green-600">
-                      Hammed Bellow
-                    </div>
-                    <div className="border-b broder-green-600">
-                      Hammed Bellow
-                    </div>
-                  </div>
-                </div>
-                <a className="absolute right-2 bottom-2">
-                  <img src="/arrow.png" alt="" className="w-4 h-4" />
-                </a>
               </div>
             </div>
-            <div className="flex flex-col justify-center w-full p-5 py-10 mt-28 mb-10 mr-5 rounded-lg bg-white text-green-800">
-              <div className="grid grid-cols-2 gap-2">
-                <div className="flex flex-col items-left font-bold">
-                  <p className="italic text-xs uppercase font-semibold p-4">
-                    Personal information
-                  </p>
+          </div>
+
+          <div className="p-5 md:w-3/4">
+            <div className="flex flex-col justify-center w-full p-5 py-5 mr-5 text-green-800 bg-white rounded-lg">
+              <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
+                <div className="flex flex-col font-bold items-left">
+                  <p className="p-4 text-xs italic font-semibold uppercase">Personal information</p>
                   <img src="/pic.jpg" alt="" className="w-2/6" />
-                  <h2 className="text-sm my-2">
+                  <h2 className="my-2 text-sm">
                     Title: <span className="uppercase">Mr</span>{" "}
                   </h2>
-                  <h2 className="text-sm my-2">
+                  <h2 className="my-2 text-sm">
                     Name: <span className="uppercase">Lekan</span>{" "}
                   </h2>
-                  <h2 className="text-sm my-2">
+                  <h2 className="my-2 text-sm">
                     Surname: <span className="uppercase">Bello</span>{" "}
                   </h2>
-                  <h2 className="text-sm my-2">
+                  <h2 className="my-2 text-sm">
                     Othernames: <span className="uppercase">odunade</span>{" "}
                   </h2>
-                  <h2 className="text-sm my-2">Gender: Male</h2>
-                  <h2 className="text-sm my-2">
-                    Date of Birth: 23 | 11 | 2021
-                  </h2>
-                  <h2 className="text-sm my-2">Phone Number: 09035919948</h2>
-                  <h2 className="text-sm my-2">
-                    Email: hbellohammed@gmail.com
-                  </h2>
-                  <h2 className="text-sm mt-2">Year of Joining PDP: 2020</h2>
+                  <h2 className="my-2 text-sm">Gender: Male</h2>
+                  <h2 className="my-2 text-sm">Date of Birth: 23 | 11 | 2021</h2>
+                  <h2 className="my-2 text-sm">Phone Number: 09035919948</h2>
+                  <h2 className="my-2 text-sm">Email: hbellohammed@gmail.com</h2>
+                  <h2 className="mt-2 text-sm">Year of Joining PDP: 2020</h2>
                 </div>
-                <div className="border-l border-green-600 font-bold text-green-800 p-4">
-                  <p className="italic text-xs uppercase font-semibold">
+                <div className="relative mt-2 font-bold text-green-800 border-green-600 md:pl-2 md:border-l">
+                  <p className="text-xs italic font-semibold uppercase">
                     Local chapter information
                   </p>
-                  <h2 className="text-sm my-2">State: Lagos</h2>
-                  <h2 className="text-sm my-2">
-                    Local Government: Lagos Mainland
-                  </h2>
-                  <h2 className="text-sm my-2">Ward: Ward B</h2>
+                  <h2 className="my-2 text-sm">State: Lagos</h2>
+                  <h2 className="my-2 text-sm">Local Government: Lagos Mainland</h2>
+                  <h2 className="my-2 text-sm">Ward: Ward B</h2>
 
-                  <p className="italic text-xs uppercase font-semibold mt-8">
-                    voting information
-                  </p>
-                  <h2 className="text-sm my-2">State: Lagos</h2>
-                  <h2 className="text-sm my-2">
-                    Local Government: Lagos Mainland
-                  </h2>
-                  <h2 className="text-sm my-2">Ward: Ward B</h2>
-                  <h2 className="text-sm my-2">Polling Unit: Polling B</h2>
-                  <h2 className="text-sm my-2">
+                  <p className="mt-8 text-xs italic font-semibold uppercase">voting information</p>
+                  <h2 className="my-2 text-sm">State: Lagos</h2>
+                  <h2 className="my-2 text-sm">Local Government: Lagos Mainland</h2>
+                  <h2 className="my-2 text-sm">Ward: Ward B</h2>
+                  <h2 className="my-2 text-sm">Polling Unit: Polling B</h2>
+                  <h2 className="my-2 text-sm">
                     VIN{" "}
-                    <span className="text-xs font-semibold">
-                      (Voters Identification Number)
-                    </span>
-                    : 2236589452
+                    <span className="text-xs font-semibold">(Voters Identification Number)</span>:
+                    2236589452
                   </h2>
-                  <h2 className="text-sm my-2">
-                    Contact Address: Bode thomas, Isale General, Oluyole, Lagos
-                    Island, Oyo State
+                  <h2 className="my-2 text-sm">
+                    Contact Address: Bode thomas, Isale General, Oluyole, Lagos Island, Oyo State
                   </h2>
-                  <h2 className="text-sm my-2">
+                  <h2 className="my-2 text-sm">
                     Former Party Position{" "}
-                    <span className="text-xs uppercase font-semibold">
-                      (If any)
-                    </span>
-                    : Chairman
+                    <span className="text-xs font-semibold uppercase">(If any)</span>: Chairman
                   </h2>
-                  <h2 className="text-sm my-2">
+                  <h2 className="my-2 text-sm">
                     Former Elected Position{" "}
-                    <span className="text-xs uppercase font-semibold">
-                      (if any)
-                    </span>
-                    : Treasurer
+                    <span className="text-xs font-semibold uppercase">(if any)</span>: Treasurer
                   </h2>
-                  <div className="mt-16 ml-80">
-                    <button className="bg-green-800 text-white font-bold rounded-xl p-2">
+                  <div className="bottom-0 flex justify-end md:absolute right-2">
+                    <button className="p-2 font-bold text-white bg-green-800 rounded-xl">
                       Done
                     </button>
                   </div>
@@ -190,4 +146,4 @@ const CallCenDashboard = () => {
   );
 };
 
-export default CallCenDashboard;
+export default AgentDashboard;
