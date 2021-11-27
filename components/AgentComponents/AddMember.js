368eb8972555855e66d980321ff5aab20268dc56
bammyt1011 <mathew.bamidele229@gmail.com>
2021-11-27 09:21:08 +0100

    form update

--- components/AgentComponents/AddMember.js
+++ components/AgentComponents/AddMember.js
@@ -10,16 +10,16 @@ const AddMember = ({ onNewAdded }) => {
         }}
         className="p-5 py-10 mt-5 rounded-lg bg-green-50"
       >
-        <h2 className="mb-6 text-xl font-bold text-green-800 uppercase md:hidden">
-          Person information
-        </h2>
-        <div className="grid grid-cols-1 gap-2 text-xs md:grid-cols-3">
+        <div className="grid grid-cols-2 gap-2 text-xs md:grid-cols-3">
           <div className="text-left">
+            <h2 className="mb-4 text-xs font-bold text-green-800 uppercase">
+              Person information
+            </h2>
             <label htmlFor="" className="w-full">
               Title <span className="font-bold text-green-600">*</span>
               <select
                 name=""
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
                 id=""
               >
                 <option value="">Please Select</option>
@@ -32,7 +32,7 @@ const AddMember = ({ onNewAdded }) => {
               Name <span className="font-bold text-green-600">*</span>
               <input
                 type="text"
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
                 placeholder="Firstname"
               />
             </label>
@@ -40,7 +40,7 @@ const AddMember = ({ onNewAdded }) => {
               Surname <span className="font-bold text-green-600">*</span>
               <input
                 type="text"
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
                 placeholder="Surname"
               />
             </label>
@@ -48,7 +48,7 @@ const AddMember = ({ onNewAdded }) => {
               Othernames <span className="font-bold text-green-600">*</span>
               <input
                 type="text"
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
                 placeholder="Othername"
               />
             </label>
@@ -56,7 +56,7 @@ const AddMember = ({ onNewAdded }) => {
               Gender <span className="font-bold text-green-600">*</span>
               <select
                 name=""
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
                 id=""
               >
                 <option value="">Please Select</option>
@@ -68,7 +68,7 @@ const AddMember = ({ onNewAdded }) => {
               Date Of Birth <span className="font-bold text-green-600">*</span>
               <input
                 type="date"
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
                 placeholder="Date of Birth"
               />
             </label>
@@ -76,7 +76,7 @@ const AddMember = ({ onNewAdded }) => {
               Phone Number <span className="font-bold text-green-600">*</span>
               <input
                 type="text"
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
                 placeholder="09012345678"
               />
             </label>
@@ -84,7 +84,7 @@ const AddMember = ({ onNewAdded }) => {
               Phone Network <span className="font-bold text-green-600">*</span>
               <select
                 name=""
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
                 id=""
               >
                 <option value="">Please Select</option>
@@ -99,14 +99,15 @@ const AddMember = ({ onNewAdded }) => {
             <img src="/profile.png" className="h-36 w-28" alt="" />
             <input
               type="file"
-              className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+              className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
               // placeholder="Upload"
             />
             <label htmlFor="">
-              Year of Joining <span className="font-bold text-green-600">*</span>
+              Year of Joining{" "}
+              <span className="font-bold text-green-600">*</span>
               <select
                 name=""
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
                 id=""
               >
                 <option value="">Please Select</option>
@@ -123,49 +124,53 @@ const AddMember = ({ onNewAdded }) => {
               Email <span className="font-bold text-green-600">*</span>
               <input
                 type="email"
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
                 placeholder="name@domain.com"
               />
             </label>
+            <h2 className="uppercase font-bold text-center my-2 text-xs text-green-800">
+              Local chapter information
+            </h2>
             <label htmlFor="">
               State <span className="font-bold text-green-600">*</span>
               <select
                 name=""
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
                 id=""
               >
                 <option value="">Please Select</option>
-                <option value="10">2015</option>
-                <option value="11">2016</option>
-                <option value="12">2017</option>
-                <option value="1">2018</option>
-                <option value="2">2019</option>
-                <option value="3">2020</option>
-                <option value="4">2021</option>
+                <option value="1">Abia</option>
+                <option value="2">Adamawa</option>
+                <option value="3">Akwa Ibom</option>
+                <option value="4">Oyo</option>
+                <option value="5">Lagos</option>
+                <option value="6">Sokoto</option>
+                <option value="7">Taraba</option>
               </select>
             </label>
             <label htmlFor="">
-              Local Government <span className="font-bold text-green-600">*</span>
+              Local Government{" "}
+              <span className="font-bold text-green-600">*</span>
               <select
                 name=""
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
                 id=""
               >
                 <option value="">Please Select</option>
-                <option value="10">2015</option>
-                <option value="11">2016</option>
-                <option value="12">2017</option>
-                <option value="1">2018</option>
-                <option value="2">2019</option>
-                <option value="3">2020</option>
-                <option value="4">2021</option>
+                <option value="1">BINJI</option>
+                <option value="2">BODINGA</option>
+                <option value="3">DANGE/SHUNI</option>
+                <option value="4">GADA</option>
+                <option value="5">GORONYO</option>
+                <option value="6">2020</option>
+                <option value="7">2021</option>
               </select>
             </label>
             <label htmlFor="">
               Ward <span className="font-bold text-green-600">*</span>
               <select
                 name=""
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
                 id=""
               >
                 <option value="">Please Select</option>
@@ -179,12 +184,15 @@ const AddMember = ({ onNewAdded }) => {
               </select>
             </label>
           </div>
-          <div className="text-left">
+          <div className="text-left col-span-full md:col-auto">
+            <h2 className="uppercase font-bold text-center my-2 text-xs text-green-800">
+              Voting information
+            </h2>
             <label htmlFor="" className="w-full">
-              Title <span className="font-bold text-green-600">*</span>
+              State <span className="font-bold text-green-600">*</span>
               <select
                 name=""
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
                 id=""
               >
                 <option value="">Please Select</option>
@@ -194,70 +202,80 @@ const AddMember = ({ onNewAdded }) => {
               </select>
             </label>
             <label htmlFor="">
-              Name <span className="font-bold text-green-600">*</span>
-              <input
-                type="text"
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
-                placeholder="Firstname"
-              />
-            </label>
-            <label htmlFor="">
-              Surname <span className="font-bold text-green-600">*</span>
-              <input
-                type="text"
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
-                placeholder="Surname"
-              />
-            </label>
-            <label htmlFor="">
-              Othernames <span className="font-bold text-green-600">*</span>
-              <input
-                type="text"
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
-                placeholder="Othername"
-              />
-            </label>
-            <label htmlFor="" className="w-full">
-              Gender <span className="font-bold text-green-600">*</span>
+              Local Government{" "}
+              <span className="font-bold text-green-600">*</span>
               <select
                 name=""
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
                 id=""
               >
                 <option value="">Please Select</option>
-                <option value="m">Male</option>
-                <option value="f">Female</option>
+                <option value="mr">Mr</option>
+                <option value="mr">Mrs</option>
+                <option value="mr">Miss</option>
               </select>
             </label>
-            <label htmlFor="" className="w-full">
-              Date Of Birth <span className="font-bold text-green-600">*</span>
-              <input
-                type="date"
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
-                placeholder="Date of Birth"
-              />
-            </label>
             <label htmlFor="">
-              Phone Number <span className="font-bold text-green-600">*</span>
-              <input
-                type="text"
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
-                placeholder="09012345678"
-              />
-            </label>
-            <label htmlFor="">
-              Phone Network <span className="font-bold text-green-600">*</span>
+              Ward <span className="font-bold text-green-600">*</span>
               <select
                 name=""
-                className="w-full p-2 mb-4 border-b border-gray-400 outline-none focus:bg-green-200"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
                 id=""
               >
                 <option value="">Please Select</option>
-                <option value="m">MTN</option>
-                <option value="a">Airtel</option>
-                <option value="e">Etisalat (9ja)</option>
-                <option value="g">GLO</option>
+                <option value="mr">Mr</option>
+                <option value="mr">Mrs</option>
+                <option value="mr">Miss</option>
               </select>
+            </label>
+            <label htmlFor="">
+              Polling Unit <span className="font-bold text-green-600">*</span>
+              <select
+                name=""
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
+                id=""
+              >
+                <option value="">Please Select</option>
+                <option value="mr">Mr</option>
+                <option value="mr">Mrs</option>
+                <option value="mr">Miss</option>
+              </select>
+            </label>
+            <label htmlFor="" className="w-full">
+              VIN (Voters Identification No)
+              <span className="font-bold text-green-600">*</span>
+              <input
+                type="text"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
+                placeholder="Voters ID Card Number"
+              />
+            </label>
+            <label htmlFor="" className="w-full">
+              Contact Address{" "}
+              <span className="font-bold text-green-600">*</span>
+              <input
+                type="text"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
+                placeholder=""
+              />
+            </label>
+            <label htmlFor="">
+              Former Party Position (IF ANY)
+              <span className="font-bold text-green-600">*</span>
+              <input
+                type="text"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
+                placeholder=""
+              />
+            </label>
+            <label htmlFor="">
+              Former Elected Position(IF ANY)
+              <span className="font-bold text-green-600">*</span>
+              <input
+                type="text"
+                className="w-full p-2 mb-4 border rounded-md border-gray-600 outline-none focus:bg-green-50"
+                placeholder=""
+              />
             </label>
           </div>
         </div>
